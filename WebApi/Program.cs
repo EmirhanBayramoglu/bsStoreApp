60ef00b3026caed261b059d363e1852d33e2510c
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-10-21 21:23:51 +0300

    Improvment of Repository LibraryClass

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using WebApi.Repositories;
-
+using Repositories.EFCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
