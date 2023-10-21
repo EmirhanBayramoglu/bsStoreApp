616a84d0cf409b042a186a188b8aac6649d555ba
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-10-21 22:54:07 +0300

    New Repositories created

---
+++ Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+            
+        }
+        //bu fonksiyonların ekstra burada yapılmasının sebebi ekstra burada book için fazladan tanım veya sorgu ekleyebilmemiz
+        //mesela getAll yaparız ama ekstra bunları Title'a göre sıralayıp çağırabiliriz
+        public void CreateOneBook(Book book) => Create(book);
+
+        public void DeleteOneBook(Book book) => Delete(book);
+
+        public IQueryable<Book> GetAll(bool trackChanges) =>
+            FindAll(trackChanges);
+
+        public IQueryable<Book> GetOneBookById(int id, bool trackChanges) =>
+            FindByCondition(x => x.Id.Equals(id), trackChanges);
+
+        public void UpdateOneBook(Book book) => Update(book);
+    }
+}
