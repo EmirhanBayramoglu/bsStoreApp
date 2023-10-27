bbbf28a0c78f1c5776993c9dc11673050833a205
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-10-27 20:07:45 +0300

    RequestParameters directory created

---
+++ Entities/RequesFeatures/RequestParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequesFeatures
+{
+    public abstract class RequestParameters
+    {
+        const int maxPageSize = 50;
+
+        //Sadece düm düz get set olduğu için buna
+        //Auto-implemented property denir
+        public int PageNumber { get; set; }
+
+        //Açıklanarak get set oluşturulmuş
+        //Full-property denir
+        private int _pageSize;
+
+        public int PageSize 
+        { 
+            get { return _pageSize; }
+            set { _pageSize = value > maxPageSize ? maxPageSize : value; } 
+        }
+    }
+}
