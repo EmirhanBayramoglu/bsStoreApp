510ea90757b15a0e5f6e9446e82f2aa0a601df7b
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-10-28 03:23:21 +0300

    Sort işlemi efektif bir hale getirildi

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -36,34 +36,8 @@ namespace Repositories.EFCore.Extensions
         {
             if(string.IsNullOrWhiteSpace(orderByQueryString))
                 return books.OrderBy(b => b.Id);
-
-            var orderParams = orderByQueryString.Trim().Split(','); //trim fazla boşlukları atmak için split de peş peşe girilen sırlama tercihleri için aradaki virgülü
-                                                                    //atıp array oluşturup ona göre sıralaması için (Title,id => array[0]=Title array[1]=id gibi)
-            var propertyInfos = typeof(Book)
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance); //book nesnesinin propertylerini alıyoruz (public ve instance olanları(newlenebilen))
-
-            var orderQueryBuilder = new StringBuilder();
-
-            foreach(var param in orderParams)
-            {
-                if(string.IsNullOrWhiteSpace(param))
-                    continue;
-
-                var propertyFromQueryName = param.Split(' ')[0]; //desc yani azdan çoğa mı a dan z ye mi onu anlamamızı sağlayacak kısım burası
-
-                var objectProperty = propertyInfos
-                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
-                    StringComparison.InvariantCultureIgnoreCase));
-
-                if(objectProperty is null)
-                    continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            
+            var orderQuery = OrderQueryBuilder.CreatOrderQuery<Book>(orderByQueryString);
 
             if(orderQuery is null)
                 return books.OrderBy(b => b.Id);
--- Repositories/EFCore/Extensions/OrderQueryBuilder.cs
+++ Repositories/EFCore/Extensions/OrderQueryBuilder.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repositories.EFCore.Extensions
 {
-    internal class OrderQueryBuilder
+    public static class OrderQueryBuilder
     {
+        public static string CreatOrderQuery<T>(string orderByQueryString)
+        {
+            var orderParams = orderByQueryString.Trim().Split(','); //trim fazla boşlukları atmak için split de peş peşe girilen sırlama tercihleri için aradaki virgülü
+                                                                    //atıp array oluşturup ona göre sıralaması için (Title,id => array[0]=Title array[1]=id gibi)
+
+            var propertyInfos = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance); //book nesnesinin propertylerini alıyoruz (public ve instance olanları(newlenebilen))
+
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(' ')[0]; //desc yani azdan çoğa mı a dan z ye mi onu anlamamızı sağlayacak kısım burası
+
+                var objectProperty = propertyInfos
+                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
+                    StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty is null)
+                    continue;
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+
+                var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+                return orderQuery;
+            }
+        }
     }
 }
